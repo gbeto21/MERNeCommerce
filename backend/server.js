4796de99c8c9fc40f74049b619bac576eb22ae11
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-01-18 08:07:35 -0600

    The env configuration was applied.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 const products = require("./data/products")
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 app.get("/", (req, res) => {
   res.send("Hi!")
@@ -15,4 +18,9 @@ app.get("/products/:id", (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log("Server running"))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} at ${PORT}`)
+)
