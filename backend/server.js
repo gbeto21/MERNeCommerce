4f644e802c49a6115720ad2aeded3399aeb5830c
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-01-18 11:28:34 -0600

    An error handler was added.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import colors from "colors"
 import connectDB from "./config/db.js"
 import productsRoutes from "./routes/productRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 const app = express()
 dotenv.config()
@@ -12,6 +13,8 @@ app.get("/", (req, res) => {
 })
 
 app.use("/products", productsRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
