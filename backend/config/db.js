c037cd75eafdb4c88ca33057015282e89e468654
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-01-18 09:43:57 -0600

    The color console was implemented.

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose"
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`)
+    console.log(`✅ MongoDB connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
-    console.error("Error connecting to the DB.", error)
+    console.error(`Error connecting to the DB. ${error}`.red.underline.bold)
     process.exit(1)
   }
 }
