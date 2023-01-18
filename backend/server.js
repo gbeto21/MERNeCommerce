16d0f30a196cddcdb0fc0dc0c07eb304806753ad
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-01-18 10:53:14 -0600

    The router for the products was implemented.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
 import connectDB from "./config/db.js"
-import products from "./data/products.js"
+import productsRoutes from "./routes/productRoutes.js"
 
 const app = express()
 dotenv.config()
@@ -11,14 +11,7 @@ app.get("/", (req, res) => {
   res.send("Hi!")
 })
 
-app.get("/products", (req, res) => {
-  res.json(products)
-})
-
-app.get("/products/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id)
-  res.json(product)
-})
+app.use("/products", productsRoutes)
 
 const PORT = process.env.PORT || 5000
 
